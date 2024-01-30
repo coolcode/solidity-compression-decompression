3f63f2073d26747e93972223dd008abe86d5577f
coolcode <coolcode@live.com>
2024-01-30 13:18:18 +0800

    🥑 hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomicfoundation/hardhat-foundry")
+require("@nomicfoundation/hardhat-toolbox")
+require("@typechain/hardhat")
+
+module.exports = {
+  solidity: {
+    version: "0.8.23",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  gasReporter: {
+    currency: "USD",
+    gasPrice: 20,
+    enabled: true,
+  },
+  paths: {
+    sources: "./src", // Use ./src rather than ./contracts as Hardhat expects
+    cache: "./cache_hardhat", // Use a different cache for Hardhat than Foundry
+  },
+}
